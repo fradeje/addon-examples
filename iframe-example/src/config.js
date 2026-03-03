eda62dd878fdf4a34523bf8e71974361fc98ff99
fradeje <jeff.frade@spainlink.es>
2026-03-03 10:23:15 +0100

    Update Config.js to point Render

--- iframe-example/src/config.js
+++ iframe-example/src/config.js
@@ -1,6 +1,6 @@
 module.exports.config = {
-  url: process.env.URL || "https://uniaxially-nonphonetic-kelsey.ngrok-free.dev",
-  port: process.env.NODE_PORT || 8080,
+  url: process.env.PUBLIC_BASE_URL || "http://localhost:8080",
+  port: Number(process.env.PORT || process.env.NODE_PORT || 8080),
   ngrok_auth_token: process.env.NGROK_AUTH_TOKEN || "",
-  manifestName: 'manifest-v0.1.json'
-}
+  manifestName: "manifest-v0.1.json",
+};
